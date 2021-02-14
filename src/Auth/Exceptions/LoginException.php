ca4aa957fe7b529bad6e8423c1f1d4da1d3f5dfe
Fatemeh Majd <fmmajd452@gmail.com>
2021-02-14 15:22:55 +0330

    added status code to LoginException

--- src/Auth/Exceptions/LoginException.php
+++ src/Auth/Exceptions/LoginException.php
@@ -7,8 +7,17 @@ use Throwable;
 
 class LoginException extends Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    private $statusCode;
+    
+    public function __construct($message = "", $code = 0, Throwable $previous = null, ?int $statusCode = null)
     {
         parent::__construct($message, $code, $previous);
+        $this->statusCode = $statusCode;
+    }
+
+    public function getStatusCode(): ?int
+    {
+        return $this->statusCode;
     }
 }
+
